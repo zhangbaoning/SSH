b5fbfa8024589dbe5309ca8b581c40ee2002d47f
张宝宁 <zhangbaoning@vip.qq.com>
2016-06-07 21:18:25 +0800

    Initial commit

---
+++ src/com/javaweb/dao/UserDAO.java
@@ -0,0 +1,16 @@
+package com.javaweb.dao;
+
+import com.javaweb.po.User;
+
+import java.util.List;
+
+/**
+ * Created by 张宝宁 on 2016/6/7.
+ */
+public interface UserDAO {
+    public void saver(User user);
+    public void delete(User user);
+    public void updata(User user);
+    public List queryAll();
+    public User queryByID(int id);
+}
